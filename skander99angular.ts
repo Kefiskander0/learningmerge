38be5edf2abca1266849c0e8d02b06fc1b568208
skanderkefi <skander.kefi@esprit.tn>
2023-04-08 15:12:12 +0100

    getting harder from master

---
+++ skander99angular.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'appppp-example',
+  template: `
+    <h1>Bienvenue sur la page d'exemple !</h1>
+    <p>{{ message }}</p>
+  `,
+})
+export class ExampleComponent implements OnInit {
+  message: string = '';
+  messagefrommaster: string
+
+  ngOnInit() {
+   // this.setMessage();
+  }
+
+  private setMessage() {
+    this.message = 'Ce texte est affiché grâce à une méthode privée.';
+  }
+}
+
